381cfda94c63aad43afa7b7c69d31f6e8447d3be
Alice <89alice.corona@gmail.com>
2016-08-04 18:30:45 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -15,13 +15,26 @@ people_links = [link.get('href') for link in root.cssselect("table tr td:nth-of-
 for link in people_links:
   html_meetings = requests.get(link).text
   root_meetings = lxml.html.fromstring(html_meetings)
-  host = root_meetings.cssselect("h3")[0].text_content().strip().encode('ascii', 'ignore')
-  host_id = link.split("host=")[1].split("-p=")[0]
+  try: 
+    host = root_meetings.cssselect("h3")[0].text_content().strip().encode('ascii', 'ignore')
+    host_id = link.split("host=")[1].split("-p=")[0]
+  except IndexError:
+    host = "something wrong"
+    host_id = "something wrong"
   print host_id
   try:
     number_pages = str(root_meetings.cssselect("span a")[-1].get('href')).split("p=")[-1]
   except IndexError: 
     number_pages = "1"
+  #pagination
+  if number page = 1:
+    member_all_meetings_links = link
+  else:
+    current page = 1
+    member_all_meetings_links = []
+  while current page <= number pages
+  if nu
+  all_meetings
   
   #//*[@id="layout"]/div/div[2]/center/span[1]/a[7]
 
