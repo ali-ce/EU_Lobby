35501d7806137d0e40108dccb34563bc810fbb8b
Alice <89alice.corona@gmail.com>
2016-08-04 18:19:34 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -4,20 +4,24 @@ import requests
 import json
 import urllib
 
+#Get links of all people involved
+
 url = "http://www.transparencyinternational.eu/european-commissions-lobbying-meetings/"
 html = requests.get(url).text
 root = lxml.html.fromstring(html)
 people_links = [link.get('href') for link in root.cssselect("table tr td:nth-of-type(3) a")]
 
+#Get info on each person
 for link in people_links:
   html_meetings = requests.get(link).text
   root_meetings = lxml.html.fromstring(html_meetings)
   host = root_meetings.cssselect("h3")[0].text_content().strip().encode('ascii', 'ignore')
+  host_id = link.split("host=")[1].split("-p=")[0]
+  print host_id
   try:
     number_pages = str(root_meetings.cssselect("span a")[-1].get('href')).split("p=")[-1]
   except IndexError: 
     number_pages = "1"
-  print number_pages
   
   #//*[@id="layout"]/div/div[2]/center/span[1]/a[7]
 
