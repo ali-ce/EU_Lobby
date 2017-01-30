4e82e50a2b73ca1aada3fd19799b41c37f590338
Alice <89alice.corona@gmail.com>
2017-01-30 20:46:01 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -38,18 +38,3 @@ for link in people_links:
   
   #//*[@id="layout"]/div/div[2]/center/span[1]/a[7]
 
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
-
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
